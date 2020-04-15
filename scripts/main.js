6c305acd1e81df8438e8af110ffaf3c431a5f319
Kira Marks <serafita22@gmail.com>
2020-04-15 15:49:51 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+
+const largeImg = document.querySelector('#largeImg');
+
+const imgReplacer = (e) => {
+  if (e.target.className.includes('gallery__thumb')) {
+    e.preventDefault();
+    largeImg.src = e.target.closest('.list-item__link');
+  }
+};
+
+thumbs.addEventListener('click', imgReplacer);
